ac9325382add30680f3c18895c5b1b7144b4549a
Naveed Ali <naveedali8086@gmail.com>
2024-02-22 00:03:30 +0500

    wip - all good except 1 error

--- src/Console/AddTraitNameCommand.php
+++ src/Console/AddTraitNameCommand.php
@@ -4,6 +4,7 @@ namespace Naveedali8086\LaravelHelpersDev\Console;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 
 class AddTraitNameCommand extends Command
 {
@@ -14,43 +15,41 @@ class AddTraitNameCommand extends Command
     public function handle()
     {
         $traitsToAdd = $this->argument('traits');
+
         $traitsToAdd = preg_replace('/\s+/', '', $traitsToAdd);
         $traitsToAdd = explode(',', $traitsToAdd);
+        $traitsToAdd = array_map(function ($trait) {
+            // first remove "\", "/" from the start of the string then replace all "\" from "/"
+            return str_replace('/', '\\', ltrim($trait, '\\/'));
+        }, $traitsToAdd);
 
-        $modelName = $this->argument('file');
-        $modelPath = $modelName;
+        $filePath = $this->argument('file');
+        // first remove "\", "/" from the start of the string then replace all "\" from "/"
+        $filePath = str_replace('/', '\\', ltrim($filePath, '\\/'));
+        $filePath = $this->getFilePath($filePath);
 
-        $modelNameArr = explode('\\', $modelName);
-        $modelName = end($modelNameArr);
-
-        // Prepend App\Models namespace if not provided
-        if (!str_contains($modelPath, '\\')) {
-            $modelPath = 'App\Models\\' . $modelName;
-        }
-        $modelFile = app_path('Models/' . $modelName . '.php');
-
-        if (!class_exists($modelPath)) {
-            $this->error("Model class $modelPath not found.");
+        if (!File::exists($filePath)) {
+            $this->error("$filePath not found.");
             return false;
         }
 
         // Read the contents of the model file
-        $contents = File::get($modelFile);
+        $contents = File::get($filePath);
 
         // Find the position of class opening bracket
         $classOpeningPosition = strpos($contents, '{');
 
-
         $traitsStart = $classOpeningPosition + 1;
         $useTraitPos = strpos($contents, 'use ', $traitsStart);
 
+        $traitNames = [];
+        foreach ($traitsToAdd as $trait) {
+            $traitParts = explode('\\', $trait);
+            $traitNames[] = end($traitParts);
+        }
+
         if ($useTraitPos === false) { // file do not have any trait at all
             // add your traits here
-            $traitNames = [];
-            foreach ($traitsToAdd as $trait) {
-                $traitParts = explode('\\', $trait);
-                $traitNames[] = end($traitParts);
-            }
             sort($traitNames);
             $updatedTraits = "\nuse " . implode(', ', $traitNames) . ';';
             // put in the file content]
@@ -59,27 +58,23 @@ class AddTraitNameCommand extends Command
         } else { // some traits already exist in file
             $traitEndingPos = strpos($contents, ';', $useTraitPos);
             $existingTraits = substr($contents, $useTraitPos, ($traitEndingPos - $useTraitPos) + 2);
-            // removing "use", "space after use" and "semicolon" at the end
-            $existingTraits = trim($existingTraits, 'use ; ');
-            $existingTraits = preg_replace('/\s+/', '', $existingTraits);
+            // removing "use", "space after use" and "semicolon"
             $existingTraits = trim(str_replace(["use", " ", ";"], "", $existingTraits));
 
             $existingTraitsArr = explode(',', $existingTraits);
-            foreach ($traitsToAdd as $trait) {
-                $traitParts = explode('\\', $trait);
-                $traitsName = end($traitParts);
+
+            foreach ($traitNames as $traitsName) {
                 if (!in_array($traitsName, $existingTraitsArr)) {
                     $existingTraitsArr[] = $traitsName;
                 }
             }
             sort($existingTraitsArr);
-            $updatedTraits = 'use ' . implode(', ', $existingTraitsArr) . ';';
+            $updatedTraits = "use " . implode(', ', $existingTraitsArr) . ';';
 
             $contents = substr_replace($contents, $updatedTraits, $useTraitPos - 1, ($traitEndingPos - $useTraitPos) + 2);
         }
 
         // adding traits imports at the top of file after sorting
-        // namespace App\Models;
         $namespacePos = strpos($contents, 'namespace');
         $namespaceSemicolonPos = strpos($contents, ';', $namespacePos);
         $classKeywordPos = strpos($contents, 'class', $namespaceSemicolonPos);
@@ -87,15 +82,12 @@ class AddTraitNameCommand extends Command
 
         $imports = explode("\n", trim($imports));
 
-        $traitsToAdd = array_map(function ($item) {
-            return "use " . $item . ";";
+        // add "use " in every element of $traitsToAdd
+        $traitsToAdd = array_map(function ($trait) {
+            return "use " . $trait . ";";
         }, $traitsToAdd);
 
-        foreach ($traitsToAdd as $trait) {
-            if (!in_array($trait, $imports)) {
-                $imports[] = $trait;
-            }
-        }
+        $imports = array_unique(array_merge($imports, $traitsToAdd));
 
         sort($imports);
 
@@ -103,10 +95,20 @@ class AddTraitNameCommand extends Command
 
         $contents = substr_replace($contents, $imports, $namespaceSemicolonPos + 1, ($classKeywordPos - $namespaceSemicolonPos) - 1);
 
-        // Write the modified contents back to the model file
-        File::put($modelFile, $contents);
+        // Write the modified contents back to the file
+        File::put($filePath, $contents);
 
-        $this->info("trait(s) has been added to the $modelName.");
+        if (windows_os()) {
+            $filePath = str_replace('/', '\\', $filePath);
+        }
+
+        $this->info(sprintf("\n Trait (%s) added to the %s", implode(', ', $traitNames), $filePath));
     }
 
+    protected function getFilePath($name): string
+    {
+        $name = Str::replaceFirst($this->laravel->getNamespace(), '', $name);
+
+        return $this->laravel['path'] . '/' . str_replace('\\', '/', $name) . '.php';
+    }
 }
