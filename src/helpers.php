61b4abe6c7ac51a1a97f7b4797773f3931792ecb
Naveed Ali <naveedali8086@gmail.com>
2024-02-27 22:28:49 +0500

    wip

--- src/helpers.php
+++ src/helpers.php
@@ -3,10 +3,13 @@
 use Illuminate\Support\Facades\File;
 
 if (!function_exists('copy_migrations')) {
-    function copy_migrations(string $destinationPath, array $filesToCopy): void
+    function copy_migrations(string $destinationPath, array|string $filePaths): void
     {
         $timestamp = null;
-        foreach ($filesToCopy as $file) {
+
+        $filePaths = is_string($filePaths) ? [$filePaths] : $filePaths;
+
+        foreach ($filePaths as $file) {
             // Extract the filename without extension
             $fileName = pathinfo($file, PATHINFO_FILENAME);
 
@@ -28,43 +31,73 @@ if (!function_exists('copy_migrations')) {
             File::copy($file, "$destinationPath/$newFilename.php");
         }
     }
+}
 
-    if (!function_exists('insertImports')) {
-        function insertImports(string &$content, array $classesNamespace): void
-        {
-            // adding imports at the top of file
-            $namespacePos = strpos($content, 'namespace');
-            $namespaceSemicolonPos = strpos($content, ';', $namespacePos);
+if (!function_exists('insertImports')) {
+    function insertImports(string &$content, array $classesNamespace): void
+    {
+        // adding imports at the top of file
+        $namespacePos = strpos($content, 'namespace');
+        $namespaceSemicolonPos = strpos($content, ';', $namespacePos);
 
-            // Get  Class' opening bracket position
-            $classKeywordPos = strpos($content, 'class', $namespaceSemicolonPos);
+        // Get  Class' opening bracket position
+        $classKeywordPos = strpos($content, 'class', $namespaceSemicolonPos);
 
-            // get existing imports as string
-            $exisingImports = substr(
-                $content,
-                $namespaceSemicolonPos + 1,
-                ($classKeywordPos - $namespaceSemicolonPos) - 1
-            );
+        // get existing imports as string
+        $exisingImports = substr(
+            $content,
+            $namespaceSemicolonPos + 1,
+            ($classKeywordPos - $namespaceSemicolonPos) - 1
+        );
 
-            $exisingImports = explode("\n", trim($exisingImports));
+        $exisingImports = explode("\n", trim($exisingImports));
 
-            // append "use " in every element of $classNamespace
-            $classesNamespace = array_map(function ($classNamespace) {
-                return "use " . $classNamespace . ";";
-            }, $classesNamespace);
+        // append "use " in every element of $classNamespace
+        $classesNamespace = array_map(function ($classNamespace) {
+            return "use " . $classNamespace . ";";
+        }, $classesNamespace);
 
-            $imports = array_unique(array_merge($exisingImports, $classesNamespace));
+        $imports = array_unique(array_merge($exisingImports, $classesNamespace));
 
-            sort($imports);
+        sort($imports);
 
-            $imports = "\n\n" . implode("\n", $imports) . "\n";
+        $imports = "\n\n" . implode("\n", $imports) . "\n";
 
-            $content = substr_replace(
-                $content,
-                $imports, $namespaceSemicolonPos + 1,
-                ($classKeywordPos - $namespaceSemicolonPos) - 1
-            );
-        }
+        $content = substr_replace(
+            $content,
+            $imports, $namespaceSemicolonPos + 1,
+            ($classKeywordPos - $namespaceSemicolonPos) - 1
+        );
     }
+}
 
+if (function_exists('create_table_migration_exists')) {
+    function create_table_migration_exists(array|string $tables): string
+    {
+        $tables = is_string($tables) ? [$tables] : $tables;
+
+        $migrationFileFoundFor = '';
+
+        $existingMigrationFiles = File::glob(database_path('migrations') . '/*.php');
+
+        foreach ($existingMigrationFiles as $migrationFile) {
+
+            $content = File::get($migrationFile);
+
+            foreach ($tables as $table) {
+
+                $pattern = '/Schema::create\s*\(\s*[\'"]' . preg_quote($table, '/') . '[\'"]\s*,\s*/i';
+                if (preg_match($pattern, $content)) {
+                    $migrationFileFoundFor = "Migration file ($migrationFile) for table ($table) already exists";
+                    break;
+                }
+            }
+
+            if ($migrationFileFoundFor) {
+                break;
+            }
+        }
+
+        return $migrationFileFoundFor;
+    }
 }
